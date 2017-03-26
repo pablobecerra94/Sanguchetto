aa0895b52338092bfa42fd011659044e33d27b55
Pablo Becerra <pablo_becerra94@yahoo.com.ar>
2017-03-25 23:02:13 -0300

    Arreglados los test de Sangucheto

--- Sangucheto/src/test/java/tallerweb/sanguchetto/modelo/SanguchettoTest.java
+++ Sangucheto/src/test/java/tallerweb/sanguchetto/modelo/SanguchettoTest.java
@@ -17,7 +17,7 @@ public class SanguchettoTest {
 	Ingrediente ingrediente;
 	Ingrediente condimento;
 	Sanguchetto sangucheto;
-/*
+
 	@Before
 	public void crear() {
 		sangucheto = Sanguchetto.getInstance();
@@ -39,8 +39,8 @@ public class SanguchettoTest {
 	@Test
 	public void testVaciar() {
 
-		sangucheto.agregarIngrediente(ingrediente);
-		sangucheto.agregarIngrediente(condimento);
+		sangucheto.agregarIngrediente(ingrediente,10);
+		sangucheto.agregarIngrediente(condimento,10);
 		sangucheto.vaciar();
 		assertTrue(sangucheto.verIngredientes().isEmpty());
 		assertTrue(sangucheto.verCondimentos().isEmpty());
@@ -49,7 +49,7 @@ public class SanguchettoTest {
 	@Test
 	public void testAgregarIngrediente() {
 		sangucheto.vaciar();
-		sangucheto.agregarIngrediente(ingrediente);
+		sangucheto.agregarIngrediente(ingrediente,10);
 		assertEquals(1, sangucheto.verIngredientes().size());
 
 	}
@@ -57,7 +57,7 @@ public class SanguchettoTest {
 	@Test
 	public void testVerIngredientes() {
 		sangucheto.vaciar();
-		sangucheto.agregarIngrediente(ingrediente);
+		sangucheto.agregarIngrediente(ingrediente,10);
 		List<Ingrediente> ingredientes = sangucheto.verIngredientes();
 		for (Ingrediente ingredienteAgregado : ingredientes) {
 			assertEquals("Milanesa", ingredienteAgregado.getNombre());
@@ -68,7 +68,7 @@ public class SanguchettoTest {
 	@Test
 	public void testVerCondimentos() {
 		sangucheto.vaciar();
-		sangucheto.agregarIngrediente(condimento);
+		sangucheto.agregarIngrediente(condimento,10);
 		List<Ingrediente> condimentos = sangucheto.verCondimentos();
 		for (Ingrediente condimentoAgregado : condimentos) {
 			assertEquals("Mayonesa", condimentoAgregado.getNombre());
@@ -79,9 +79,9 @@ public class SanguchettoTest {
 	@Test
 	public void testGetPrecio() {
 		sangucheto.vaciar();
-		sangucheto.agregarIngrediente(ingrediente);
-		sangucheto.agregarIngrediente(condimento);
+		sangucheto.agregarIngrediente(ingrediente,1);
+		sangucheto.agregarIngrediente(condimento,1);
 		Double valorEsperado = 11.5;
 		assertEquals(valorEsperado, sangucheto.getPrecio());
-	}*/
+	}
 }
